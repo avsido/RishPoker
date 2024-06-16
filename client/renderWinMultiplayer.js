63fc282edd1f940c85b3b376c623e8e0b7a8f8d3
97252 <avsido1988@gmail.com>
2024-06-16 23:46:01 +0300

    almost there with player B animations

--- client/renderWinMultiplayer.js
+++ client/renderWinMultiplayer.js
@@ -9,15 +9,18 @@ function renderWinMultiplayer(index) {
 
   let playerCards;
   let opponentCards;
+  let lose;
 
   if (currentGame.player == "a") {
     playerCards = currentGame.playerACards;
     opponentCards = currentGame.playerBCards;
+    lose = -1;
   }
 
   if (currentGame.player == "b") {
     playerCards = currentGame.playerBCards;
     opponentCards = currentGame.playerACards;
+    lose = 1;
   }
 
   let hPlayerB = document.createElement("h2");
@@ -32,40 +35,44 @@ function renderWinMultiplayer(index) {
     for (let j = 0; j < opponentCards[i].length; j++) {
       let imgCard = document.createElement("img");
       imgCard.style.zIndex += 1;
-      imgCard.src =
-        j > 3
-          ? "images/anon_card.png"
-          : "images/" + opponentCards[i][j].name.toLowerCase() + ".png";
+      imgCard.src = "images/" + opponentCards[i][j].name.toLowerCase() + ".png";
       if (j > 3) {
+        imgCard.src =
+          index > i
+            ? "images/" + opponentCards[i][j].name.toLowerCase() + ".png"
+            : "images/anon_card.png";
         if (index == i) {
+          // if hand loses RIGHT NOW
           setTimeout(() => {
             imgCard.classList.add("imgCardAnimated");
             if (soundOn) flipOpponentCardSound.play();
             imgCard.src =
               "images/" + opponentCards[i][j].name.toLowerCase() + ".png";
           }, 2);
-          if (socketWinArr[i].winner == -1) {
+          if (socketWinArr[i].winner == lose) {
             setTimeout(() => {
               cardDiv.classList.add("cardDivPlayerBLostAnimated");
             }, 500);
-
-            // This will only happen for the last hand should the computer loose
-            // This is a special case because we usually draw this _after_
-            if (i == 4) {
-              setTimeout(() => {
-                cardDiv.classList.add("cardDivPlayerBLostFinal");
-              }, 2000);
-            }
+          }
+          // This will only happen for the last hand should the computer loose
+          // This is a special case because we usually draw this _after_
+          // if (i == 4) {
+          //   setTimeout(() => {
+          //     cardDiv.classList.add("cardDivPlayerBLostFinal");
+          //   }, 2000);
+          // }
+          // }
+        } else {
+          // This will happen in the NEXT iteration of i
+          if (socketWinArr[i].winner == lose && index > i) {
+            // if hand loses at all
+            imgCard.src =
+              "images/" + opponentCards[i][j].name.toLowerCase() + ".png";
+            cardDiv.classList.add("cardDivPlayerBLostFinal");
           }
         }
-
-        //  else {
-        //   // This will happen in the NEXT iteration of i
-        //   if (socketWinArr[i].winner == -1) {
-        //     cardDiv.classList.add("cardDivPlayerBLostFinal");
-        //   }
-        // }
       }
+
       cardDiv.appendChild(imgCard);
     }
 
